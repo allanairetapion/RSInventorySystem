f8df6d3bf1c75295bf5be624d20c13b18ea04e3c
atkingjeyyms <jamesmonreal.jm@gmail.com>
2016-05-04 08:28:23 +0800

    controllersAndNames
    renaming folders, controllers, etc.

---
+++ app/Http/Controllers/inventorySysController.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class inventorySysController extends Controller{
+	/**
+	 * Show the login page for the given user
+	 * 
+	 */
+	 public function showIndex(){
+	 	return view("inventory.index");
+	 }
+	 
+	/**
+	 * Show the dashboard page for the given user
+	 * 
+	 */
+	 
+	  
+}
